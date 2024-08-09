3a6c9bdee86f3a7b262943b36351500372b4a11e
Jennifer <thalitabrasil872@gmail.com>
2024-08-09 13:18:48 -0400

    change in the display of letters

--- src/components desktop/LyricsDesktop.jsx
+++ src/components desktop/LyricsDesktop.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useMemo } from "react";
 import "./LyricsDesktop.css";
-import data from "../data/listMusics.json"; 
+import data from "../data/listMusics.json";
 import { useParams } from "react-router-dom";
 
 const LyricsDesktop = () => {
@@ -11,34 +11,49 @@ const LyricsDesktop = () => {
     }, []);
 
     useEffect(() => {
-        
         const parsedIndex = parseInt(index);
         if (isNaN(parsedIndex) || parsedIndex < 0 || parsedIndex >= sortedData.length) {
-           
             console.error("Índice inválido ou não encontrado");
             return;
         }
 
-        
         const letra = sortedData[parsedIndex].letra;
         const paragrafos = letra.split(/\n{2,}/).map((paragrafo, index) => (
             <p key={index}>{paragrafo.split('\n').map((linha, i) => <React.Fragment key={i}>{i > 0 ? <br /> : null}{linha}</React.Fragment>)}</p>
         ));
 
-        
-        setLyrics(paragrafos);
+        // Divida os parágrafos em grupos de 4
+        const groupedParagrafos = [];
+        for (let i = 0; i < paragrafos.length; i += 4) {
+            groupedParagrafos.push(paragrafos.slice(i, i + 4));
+        }
+
+        setLyrics(groupedParagrafos);
     }, [index, sortedData]);
 
     if (!lyrics) {
         return <div className="container-lyrics-desktop">Carregando...</div>;
     }
 
-    
-
     return (
         <div className="container-lyrics1">
             <h2>{sortedData[parseInt(index)].titulo}</h2>
-            {lyrics}
+            <div className="container-lyrics1-1">
+                <div className="left-column">
+                    {lyrics.map((group, idx) => idx % 2 === 0 && (
+                        <div key={idx} className="group">
+                            {group}
+                        </div>
+                    ))}
+                </div>
+                <div className="right-column">
+                    {lyrics.map((group, idx) => idx % 2 !== 0 && (
+                        <div key={idx} className="group">
+                            {group}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
--- src/components mobile/Lyrics.jsx
+++ src/components mobile/Lyrics.jsx
@@ -30,7 +30,7 @@ const Lyrics = () => {
     }, [index, sortedData]);
 
     if (!lyrics) {
-        return <div className="container-lyrics1">Carregando...</div>;
+        return <div className="container-lyrics-mobile">Carregando...</div>;
     }
 
     
